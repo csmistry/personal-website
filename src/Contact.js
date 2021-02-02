65d2a0e5b093ddb2a4e8b4aaa483d1956abb2726
Chintan Mistry <csmistry@uwaterloo.ca>
2021-02-02 00:03:07 -0500

    added contact section links

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import './components/Layout.css'
+import github from './assets/github_logo.png';
+import linkedin from './assets/linkedin_logo.png';
+import email from './assets/email_icon.svg';
 
 export const Contact = () => (     
     <div id="contact" className="layout-element">
         <h1 className="section-heading">Contact Me</h1>
-        <p>this section includes links to contact you Github, Instagram, LinkedIn ... </p>
+        <Container>
+            <a href="https://github.com/csmistry">
+                <img  src={github} alt='github' className="githubLogo"/>
+            </a>
+            <a href="https://www.linkedin.com/in/chintanmistry17">
+                <img src={linkedin} alt='linkedin' className="linkedinLogo"/>
+            </a>
+            <a >
+                <img src={email} alt='email' className="emailIcon"/>
+            </a>
+        </Container>
     </div>
 )
