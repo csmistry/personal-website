23171f7a585624cf43183b07ead6fb076fd5b782
Chintan Mistry <csmistry@uwaterloo.ca>
2021-02-04 22:37:51 -0500

    added click to copy email to clip board

--- src/Contact.js
+++ src/Contact.js
@@ -5,19 +5,32 @@ import github from './assets/github_logo.png';
 import linkedin from './assets/linkedin_logo.png';
 import email from './assets/email_icon.svg';
 
-export const Contact = () => (     
-    <div id="contact" className="layout-element">
+
+export const Contact = () => {     
+  
+  const copyToClipboard = () => { 
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = "chintan.mistry76@gmail.com";
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+  }
+
+   return (
+   <div id="contact" className="layout-element">
         <h1 className="section-heading">Contact Me</h1>
         <Container>
-            <a href="https://github.com/csmistry">
+            <a target="_blank" href="https://github.com/csmistry">
                 <img  src={github} alt='github' className="githubLogo"/>
             </a>
-            <a href="https://www.linkedin.com/in/chintanmistry17">
+            <a target="_blank" href="https://www.linkedin.com/in/chintanmistry17">
                 <img src={linkedin} alt='linkedin' className="linkedinLogo"/>
             </a>
             <a >
-                <img src={email} alt='email' className="emailIcon"/>
+                <img onClick={copyToClipboard} src={email} alt='email' className="emailIcon"/>
             </a>
         </Container>
     </div>
-)
+   );
+};
