85e96d8149ea8a7592b6a29e12ddfaeedd43665b
Chintan Mistry <csmistry@uwaterloo.ca>
2021-02-04 23:14:12 -0500

    updated jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,8 +6,8 @@ export const Jumbotron = () => (
     <Jumbo fluid className="jumbo">
         <div className="overlay"></div>
         <Container>
-            <h1>Welcome</h1>
-            <p>this is my portfolio website</p>
+            <h1>Welcome!</h1>
+            <p>Keep scrolling to learn more :)</p>
         </Container>
     </Jumbo>
 
