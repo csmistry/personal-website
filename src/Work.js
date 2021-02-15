e8a729961bc14397a0e4dda34407baa07ac042f5
Chintan Mistry <csmistry@uwaterloo.ca>
2021-02-15 11:57:12 -0500

    updated cards in work section. added badges for dates

--- src/Work.js
+++ src/Work.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './components/Layout.css'
+import './work.css'
 import awn from './assets/awn.png'
 import manulife from './assets/manulife.png'
 import novx from './assets/novx.png'
 import coreavi from './assets/coreavi.png'
+import Badge from 'react-bootstrap/Badge'
 
 export const Work = () => (     
     <div id="work" className="layout-element">
@@ -11,29 +13,36 @@ export const Work = () => (
         <div class="card">
             <img src={awn} className="awn-logo"/>
             <div class="container">
-                <h4><b>Software Developer</b></h4> 
-                <p>Risk Platform - Backend</p> 
+                <h4><b>Software Developer</b></h4>
+                <p>Risk Platform - Backend </p>
+                <h5><Badge className="date-badge">Jan. 2021 - Apr. 2021</Badge></h5>      
             </div>
         </div>
+        <hr/>
         <div class="card">
-            <img src={manulife} className="awn-logo"/>
+            <img src={manulife} className="manulife-logo"/>
             <div class="container">
                 <h4><b>Software Engineering Intern</b></h4> 
-                <p>Group Benefit Claims Division - Backend</p> 
+                <p>Group Benefit Claims Division - Backend</p>
+                <h5><Badge className="date-badge">May 2020 - Aug. 2020</Badge></h5>
             </div>
         </div>
+        <hr/>
         <div class="card">
-            <img src={novx} className="awn-logo"/>
+            <img src={novx} className="novx-logo"/>
             <div class="container">
-                <h4><b>Mobile Software Engineering Intern</b></h4> 
-                <p>Frontend UI</p> 
+                <h4><b>Software Engineering Intern</b></h4> 
+                <p>Mobile - UI</p> 
+                <h5><Badge className="date-badge">Sept. 2019 - Dec. 2019</Badge></h5>
             </div>
         </div>
+        <hr/>
         <div class="card">
-            <img src={coreavi} className="awn-logo"/>
+            <img src={coreavi} className="coreavi-logo"/>
             <div class="container">
                 <h4><b>DevOps Engineering Intern</b></h4> 
                 <p>System Admin - Automation</p> 
+                <h5><Badge className="date-badge">Jan. 2019 - Apr. 2019</Badge></h5>
             </div>
         </div>
     </div>
