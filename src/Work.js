29c6298217a24ae05b67b8d5dd31ac1a67103be0
Chintan Mistry <csmistry@uwaterloo.ca>
2021-02-17 20:36:44 -0500

    bolding About Me section. Adding div strucutre for work section

--- src/Work.js
+++ src/Work.js
@@ -10,40 +10,87 @@ import Badge from 'react-bootstrap/Badge'
 export const Work = () => (     
     <div id="work" className="layout-element">
         <h1 className="section-heading">Work Experience</h1>
-        <div class="card">
-            <img src={awn} className="awn-logo"/>
-            <div class="container">
-                <h4><b>Software Developer</b></h4>
-                <p>Risk Platform - Backend </p>
-                <h5><Badge className="date-badge">Jan. 2021 - Apr. 2021</Badge></h5>      
+        
+        <div className="work-element">
+            <div className="work-horizontal-flexbox">
+                <div className="card">
+                    <img src={awn} className="awn-logo"/>
+                    <div className="container">
+                        <h4><b>Software Developer</b></h4>
+                        <p>Risk Platform - Backend </p>
+                        <h5><Badge className="date-badge">Jan. 2021 - Apr. 2021</Badge></h5>      
+                    </div>
+                </div>
+                <div className="work-overview">
+
+                </div>
+            </div>
+            <div className="tech-stack">
+
             </div>
         </div>
+
         <hr/>
-        <div class="card">
-            <img src={manulife} className="manulife-logo"/>
-            <div class="container">
-                <h4><b>Software Engineering Intern</b></h4> 
-                <p>Group Benefit Claims Division - Backend</p>
-                <h5><Badge className="date-badge">May 2020 - Aug. 2020</Badge></h5>
+
+        <div className="work-element">
+            <div className="work-horizontal-flexbox">
+                <div className="card">
+                    <img src={manulife} className="manulife-logo"/>
+                    <div className="container">
+                        <h4><b>Software Engineering Intern</b></h4> 
+                        <p>Group Benefit Claims Division - Backend</p>
+                        <h5><Badge className="date-badge">May 2020 - Aug. 2020</Badge></h5>
+                    </div>
+                </div>
+                <div className="work-overview">
+
+                </div>
+            </div>
+            <div className="tech-stack">
+                
             </div>
         </div>
+
         <hr/>
-        <div class="card">
-            <img src={novx} className="novx-logo"/>
-            <div class="container">
-                <h4><b>Software Engineering Intern</b></h4> 
-                <p>Mobile - UI</p> 
-                <h5><Badge className="date-badge">Sept. 2019 - Dec. 2019</Badge></h5>
+
+        <div className="work-element"> 
+            <div className="work-horizontal-flexbox">
+                <div className="card">
+                    <img src={novx} className="novx-logo"/>
+                    <div className="container">
+                        <h4><b>Software Engineering Intern</b></h4> 
+                        <p>Mobile - UI</p> 
+                        <h5><Badge className="date-badge">Sept. 2019 - Dec. 2019</Badge></h5>
+                    </div>
+                </div>
+                <div className="work-overview">
+
+                </div>
             </div>
-        </div>
+            <div className="tech-stack">
+            </div>  
+        </div>    
+
         <hr/>
-        <div class="card">
-            <img src={coreavi} className="coreavi-logo"/>
-            <div class="container">
-                <h4><b>DevOps Engineering Intern</b></h4> 
-                <p>System Admin - Automation</p> 
-                <h5><Badge className="date-badge">Jan. 2019 - Apr. 2019</Badge></h5>
+        
+        <div className="work-element">
+            <div className="work-horizontal-flexbox">   
+                <div className="card">
+                    <img src={coreavi} className="coreavi-logo"/>
+                    <div className="container">
+                        <h4><b>DevOps Engineering Intern</b></h4> 
+                        <p>System Admin - Automation</p> 
+                        <h5><Badge className="date-badge">Jan. 2019 - Apr. 2019</Badge></h5>
+                    </div>
+                </div>
+                <div className="work-overview">
+
+                </div>
+            </div>
+            <div className="tech-stack">
+
             </div>
         </div>
+
     </div>
 )
