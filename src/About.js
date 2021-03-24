bc581cb2d5e3ed0705a0efec5913f2b521c3cd9e
Chintan Mistry <csmistry@uwaterloo.ca>
2021-03-23 23:30:13 -0400

    added first set of pics to about me

--- src/About.js
+++ src/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './components/Layout.css'
 import wloo from './assets/wlooEng.png'
 import './about.css'
+import myPic from './assets/myFace.jpg'
 
 
 export const About = () => (     
@@ -9,15 +10,21 @@ export const About = () => (
             <h1 className="section-heading">About Me </h1>
             <div>
               <h5>Hey there! My name is Chintan and welcome to my website. Keep reading to learn more about me and some of my past experiences.</h5>
-              <div>
-                  <ul className="list">
-                    <h6><li className="li-abt">Starting my 3rd year of Computer Engineering at University of Waterloo</li></h6>
-                    <h6><li className="li-abt">An avid learner who's always looking for new ways to leverage technology to make our lives easier</li></h6>
-                    <h6><li className="li-abt">I've gained a wide variety of both techincal and interpersonal skills through previous internships</li></h6>
-                    <h6><li className="li-abt">Enjoy working with others to create effective solutions for complex problems</li></h6>
-                    <h6><li className="li-abt">Interested in both Backend & Full-Stack Development</li></h6>
-                    <h6><li className="li-abt">Proficient in C++, C and Golang</li></h6>                                  
-                  </ul>
+              <div className="about-horizontal-flexbox">
+                <div>
+                    <ul className="list">
+                      <h6><li className="li-abt">Starting my 3rd year of Computer Engineering at University of Waterloo</li></h6>
+                      <h6><li className="li-abt">An avid learner who's always looking for new ways to leverage technology to make our lives easier</li></h6>
+                      <h6><li className="li-abt">I've gained a wide variety of both techincal and interpersonal skills through previous internships</li></h6>
+                      <h6><li className="li-abt">Enjoy working with others to create effective solutions for complex problems</li></h6>
+                      <h6><li className="li-abt">Interested in both Backend & Full-Stack Development</li></h6>
+                      <h6><li className="li-abt">Proficient in C++, C and Golang</li></h6>                                  
+                    </ul>
+                </div>
+                <div>
+                  <img src={myPic} alt="Avatar" className="myPic-img"/>
+                  <img className="wloo-img" src={wloo} alt="Avatar"/>
+                </div>
               </div>
               <h5>Things I enjoy outside of work and school ...</h5>
               <div>
@@ -32,3 +39,8 @@ export const About = () => (
             </div>
           </div>
 )
+
+/*
+                  <img className="myPic-img" src={myPic} alt="Avatar"/>
+                
+                  */
