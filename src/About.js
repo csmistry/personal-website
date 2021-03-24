800596e5b245e6040490f15d0148e1e63fc13aca
Chintan Mistry <csmistry@uwaterloo.ca>
2021-03-23 23:55:38 -0400

    fixing images in about me section

--- src/About.js
+++ src/About.js
@@ -3,6 +3,8 @@ import './components/Layout.css'
 import wloo from './assets/wlooEng.png'
 import './about.css'
 import myPic from './assets/myFace.jpg'
+import skating from './assets/meSkating.jpg'
+import Backend from './assets/backend.jpeg'
 
 
 export const About = () => (     
@@ -27,20 +29,20 @@ export const About = () => (
                 </div>
               </div>
               <h5>Things I enjoy outside of work and school ...</h5>
-              <div>
-                  <ul className="list">
-                    <h6><li className="li-abt">During Summer, finding hiking trails with great views!</li></h6>
-                    <h6><li className="li-abt">During Winter, Ice Skating as much as I can :)</li></h6>
-                    <h6><li className="li-abt">Top 3 shows: Game of Thrones, Silicon Valley, The Office</li></h6>
-                    <h6><li className="li-abt">Staying informed about new emerging tech and current isssues in the tech space</li></h6>
-                    <h6><li className="li-abt">Building things with new tools and frameworks during Hackathons</li></h6>                              
-                  </ul>
+              <div className="about-horizontal-flexbox">
+                <div>
+                    <ul className="list">
+                      <h6><li className="li-abt">During Summer, finding hiking trails with great views!</li></h6>
+                      <h6><li className="li-abt">During Winter, Ice Skating as much as I can :)</li></h6>
+                      <h6><li className="li-abt">Top 3 shows: Game of Thrones, Silicon Valley, The Office</li></h6>
+                      <h6><li className="li-abt">Staying informed about new emerging tech and current isssues in the tech space</li></h6>
+                      <h6><li className="li-abt">Building things with new tools and frameworks during Hackathons</li></h6>                              
+                    </ul>
+                </div>
+                <div>
+                  <img src={Backend} alt="Avatar" className="backend-img"/>
+                </div>
               </div>
             </div>
           </div>
 )
-
-/*
-                  <img className="myPic-img" src={myPic} alt="Avatar"/>
-                
-                  */
